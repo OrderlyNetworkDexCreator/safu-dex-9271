65de3c514161501121fae173dff2dc5d2f4aebc2
Mario Reder <mario.reder@pm.me>
2025-10-04 19:36:09 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/FOURDEX_BSC",
   "VITE_SEO_SITE_NAME": "FOUR DEX",
   "VITE_SEO_SITE_DESCRIPTION": "The ultimate leverage trading hub on Binance Smart Chain.",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/safu-dex-9271/",
+  "VITE_SEO_SITE_URL": "https://fourdex.io",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@FOURDEX_BSC",
